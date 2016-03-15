918ce4b76253e48cda97a1b2b88f0393b3c9e547
MarcelTHI <marcel@familiethiele.de>
2016-03-15 20:19:19 +0100

    fixed

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -4,15 +4,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Deine mama");
-<<<<<<< HEAD
 		
 		int x = 0;
+
 		
 		int y = x + 5;
 		
 		System.out.println("Das wurde auf dem Notebook geschrieben");
-=======
->>>>>>> parent of 122e234... int x added from iMac 
 	}
 
 }
